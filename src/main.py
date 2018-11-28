668142df50003782c7baefd33cce88f5b66e6f29
Elijah <elijah.blake@aggiemail.usu.edu>
2018-11-28 16:50:06 -0700

    I removed the prompt for alternate file extensions. All will be made a .bmp files

--- src/main.py
+++ src/main.py
@@ -9,18 +9,6 @@ def getSize(config_dict):
     return config_dict["pixels:"]
 
 
-def getFileExtension():
-    getting_type = True
-    while getting_type:
-        extension_name = input("\nWhat file type would\nyou like to save the image as?\n")
-        if extension_name == ".png" or extension_name == ".gif":
-            print("You selected " + extension_name)
-            getting_type = False
-        else:
-            print("Supported file extensions are .png or .gif\n")
-    return extension_name
-
-
 def getGradient(config_dictionary):
     steps = int(config_dictionary["iterations:"])
     gradient = GradientFactory.makeGradient()
@@ -37,7 +25,6 @@ def nameImage():
 image = nameImage()
 config_dict = Config.getConfigDict()
 img_size = int(getSize(config_dict))
-file_extension = getFileExtension()
 myGradient = getGradient(config_dict)
 
 # Generate the image
@@ -50,6 +37,6 @@ canvas.pack()
 canvas.create_image((img_size//2, img_size//2), image=img, state="normal")
 
 # Save the picture to a file
-img.write(image + file_extension)
-print(f"Wrote picture {image}.gif")
+img.write(image + ".bmp")
+print(f"Wrote picture {image}.bmp")
 mainloop()
