1e8c4d644b722abfae9b48bdf50860498a623f1e
Elijah <elijah.blake@aggiemail.usu.edu>
2018-11-28 12:47:00 -0700

    Added a new frac file to use for the mandelbrot3 fractal

--- src/FractalFactory.py
+++ src/FractalFactory.py
@@ -1,23 +1,20 @@
 import mandelbrot
 import mandelbrot3
 import julia
-import random
 
 def makeFractal(config_dict):
     # Make and return a fractal object
-    witch = random.randint(0, 2)
     type = config_dict["type:"].lower()
     if type == "mandelbrot":
-
-        if witch >= 0:
-            print("Mandelbrot fractal picked")
-            m = mandelbrot.Mandelbrot()
-        else:
-            print("Mandelbrot3 fractal picked")
-            m = mandelbrot3.mandelbrot3()
+        print("Mandelbrot fractal picked")
+        m = mandelbrot.Mandelbrot()
         return m
+    elif type == "mandelbrot3":
+        print("Mandelbrot3 fractal picked")
+        m3 = mandelbrot3.mandelbrot3()
+        return m3
     else:
         j = julia.julia()
         print("julia fractal picked")
         return j
-        
+
